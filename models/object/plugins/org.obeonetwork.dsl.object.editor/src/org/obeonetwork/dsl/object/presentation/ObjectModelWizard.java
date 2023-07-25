a2402fdfe9eb48a8c73a261dfe4cd8df0c83a260
Vincent Richard <vincent.richard@obeo.fr>
2023-07-25 19:26:40 +0200

    [connect] Object MM - Fix GenModel Copyright value

--- models/object/plugins/org.obeonetwork.dsl.object.editor/src/org/obeonetwork/dsl/object/presentation/ObjectModelWizard.java
+++ models/object/plugins/org.obeonetwork.dsl.object.editor/src/org/obeonetwork/dsl/object/presentation/ObjectModelWizard.java
@@ -1,5 +1,12 @@
 /**
- * Copyright Text	Copyright (c) 2008, 2023 Obeo....
+ * Copyright (c) 2008, 2023 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
  */
 package org.obeonetwork.dsl.object.presentation;
 
@@ -399,7 +406,8 @@ public class ObjectModelWizard extends Wizard implements INewWizard {
 		 */
 		@Override
 		public void createControl(Composite parent) {
-			Composite composite = new Composite(parent, SWT.NONE); {
+			Composite composite = new Composite(parent, SWT.NONE);
+			{
 				GridLayout layout = new GridLayout();
 				layout.numColumns = 1;
 				layout.verticalSpacing = 12;
