61ee2ce5fa4811b17e709782d67a948363c2e04c
Vincent Richard <vincent.richard@obeo.fr>
2023-08-09 20:48:28 +0200

    [connect] Object Workspace Diagram - Show properties as bordered nodes or subnodes depending on their type

--- designs/object/plugins/org.obeonetwork.dsl.object.design/src/org/obeonetwork/dsl/object/design/services/PropertyValueService.java
+++ designs/object/plugins/org.obeonetwork.dsl.object.design/src/org/obeonetwork/dsl/object/design/services/PropertyValueService.java
@@ -1,16 +1,34 @@
 package org.obeonetwork.dsl.object.design.services;
 
+import org.obeonetwork.dsl.environment.Attribute;
+import org.obeonetwork.dsl.environment.Reference;
+import org.obeonetwork.dsl.object.DataTypeValue;
+import org.obeonetwork.dsl.object.ObjectValue;
 import org.obeonetwork.dsl.object.PropertyValue;
 
 public class PropertyValueService {
 	
 	public static boolean isDataTypePropertyValue(PropertyValue propertyValue) {
-		// TODO
-		return true;
+		if(propertyValue.getMetaProperty() != null) {
+			return propertyValue instanceof Attribute;
+		}
+		
+		if(!propertyValue.getValues().isEmpty()) {
+			return propertyValue.getValues().get(0) instanceof DataTypeValue;
+		}
+		
+		return false;
 	}
 
 	public static boolean isObjectPropertyValue(PropertyValue propertyValue) {
-		// TODO
+		if(propertyValue.getMetaProperty() != null) {
+			return propertyValue instanceof Reference;
+		}
+		
+		if(!propertyValue.getValues().isEmpty()) {
+			return propertyValue.getValues().get(0) instanceof ObjectValue;
+		}
+		
 		return true;
 	}
 
