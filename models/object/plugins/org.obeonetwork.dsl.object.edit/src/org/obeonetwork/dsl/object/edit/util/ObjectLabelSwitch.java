3a50339a202fb17640e99d229fa326fdbab78e14
Vincent Richard <vincent.richard@obeo.fr>
2023-08-15 19:00:24 +0200

    [connect] Object MM - ObjectLabelSwitch

--- models/object/plugins/org.obeonetwork.dsl.object.edit/src/org/obeonetwork/dsl/object/edit/util/ObjectLabelSwitch.java
+++ models/object/plugins/org.obeonetwork.dsl.object.edit/src/org/obeonetwork/dsl/object/edit/util/ObjectLabelSwitch.java
@@ -1,12 +1,17 @@
 package org.obeonetwork.dsl.object.edit.util;
 
+import java.util.Arrays;
 import java.util.Objects;
 
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.obeonetwork.dsl.object.LiteralValue;
+import org.obeonetwork.dsl.object.ObjectPackage;
 import org.obeonetwork.dsl.object.ObjectValue;
 import org.obeonetwork.dsl.object.PrimitiveTypeValue;
+import org.obeonetwork.dsl.object.PropertyValue;
 import org.obeonetwork.dsl.object.Value;
+import org.obeonetwork.dsl.object.Workspace;
 import org.obeonetwork.dsl.object.util.ObjectSwitch;
 
 public class ObjectLabelSwitch extends ObjectSwitch<String> {
@@ -17,24 +22,75 @@ public class ObjectLabelSwitch extends ObjectSwitch<String> {
 		return (value.getMetaType() != null)? value.getMetaType().getName() : "---";
 	}
 	
-	@Override
-	public String caseObjectValue(ObjectValue object) {
-		// aql:': ' + if self.metaType <> null then self.metaType.name else '---' endif
-		return ": " + getValueTypeName(object);
-	}
-	
-	@Override
-	public String casePrimitiveTypeValue(PrimitiveTypeValue object) {
-		// aql:self.value.toString() + ' : ' + if self.metaType <> null then self.metaType.name else '---' endif
-		return Objects.toString(object.getData()) + " : " + getValueTypeName(object);
+	private String getObjectValueNameGuess(ObjectValue objectValue) {
+		return Arrays.asList("name", "key").stream()
+		.flatMap(name -> objectValue.getPropertyValues().stream()
+				.filter(pv -> name.equalsIgnoreCase(pv.getName())))
+		.map(pv -> getValuesLabel(pv.getValues(), pv.isMultiple()))
+		.findFirst().orElse("<ObjectValue>");
 	}
 
 	@Override
-	public String caseLiteralValue(LiteralValue object) {
-		// aql:self.name + ' : ' + if self.metaType <> null then self.metaType.name else '---' endif
-		return object.getName() + " : " + getValueTypeName(object);
+	public String caseObjectValue(ObjectValue objectValue) {
+		return getObjectValueNameGuess(objectValue) + " : " + getValueTypeName(objectValue);
 	}
 	
+	@Override
+	public String casePrimitiveTypeValue(PrimitiveTypeValue primitiveTypeValue) {
+		return getShortValueLabel(primitiveTypeValue) + " : " + getValueTypeName(primitiveTypeValue);
+	}
+
+	@Override
+	public String caseLiteralValue(LiteralValue literalValue) {
+		return getShortValueLabel(literalValue) + " : " + getValueTypeName(literalValue);
+	}
+	
+	
+	@Override
+	public String casePropertyValue(PropertyValue propertyValue) {
+		return propertyValue.getName() + " = " + getValuesLabel(propertyValue.getValues(), propertyValue.isMultiple());
+	}
+
+	private String getValuesLabel(EList<Value> values, boolean multiple) {
+		if(!multiple) {
+			return (values.isEmpty())? 
+				"()" :
+				getShortValueLabel(values.get(0));
+		} else {
+			StringBuffer label = new StringBuffer();
+			label.append("{");
+			if(!values.isEmpty()) {
+				label.append(getShortValueLabel(values.get(0)));
+			}
+			if(values.size() > 1) {
+				label.append(",...");
+			}
+			label.append("}");
+			return label.toString();
+		}
+	}
+
+	private String getShortValueLabel(Value value) {
+		String label = null;
+		switch(value.eClass().getClassifierID()) {
+		case ObjectPackage.PRIMITIVE_TYPE_VALUE:
+			label = Objects.toString(((PrimitiveTypeValue)value).getData());
+			break;
+		case ObjectPackage.LITERAL_VALUE:
+			label = ((LiteralValue)value).getName();
+			break;
+		case ObjectPackage.OBJECT_VALUE:
+			label = getObjectValueNameGuess((ObjectValue) value);
+			break;
+		}
+		return label;
+	}
+
+	@Override
+	public String caseWorkspace(Workspace workspace) {
+		return workspace.getName();
+	}
+
 	public static String getObjectLabel(EObject eObject) {
 		return INSTANCE.doSwitch(eObject);
 	}
