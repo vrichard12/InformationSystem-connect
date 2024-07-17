1d036d42a34e588a0a315ebacae9f8cef93f4af0
Vincent Richard <vincent.richard@obeo.fr>
2024-07-17 14:09:28 +0200

    [connect] SmartEA Meta Model import - UI and backend skeleton

---
+++ addons/smartea/plugins/org.obeonetwork.dsl.soa.smartea/src/org/obeonetwork/dsl/soa/smartea/SmartEAMetaModelImporter.java
@@ -0,0 +1,19 @@
+package org.obeonetwork.dsl.soa.smartea;
+
+import java.util.List;
+
+import org.eclipse.emf.ecore.EClass;
+import org.obeonetwork.dsl.soa.System;
+
+public class SmartEAMetaModelImporter {
+
+	public SmartEAMetaModelImporter(System soaSystem) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer importEClasses(List<EClass> eClasses) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
